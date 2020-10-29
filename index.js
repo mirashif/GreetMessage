9776e3a0d53b14ae7dbadacafd6b2063e0510124
Mir Ashif <34467318+mirashif@users.noreply.github.com>
2020-10-30 01:29:12 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function greet() {
+  const hours = new Date().getHours();
+
+  if (hours < 12) return 'Good Morning';
+  else if (hours >= 12 && hours <= 17) return 'Good Afternoon';
+  else if (hours >= 17 && hours <= 24) return 'Good Evening';
+
+  return null;
+}
